31c240f61fb261d60b9f8f8f75183aa166d29583
iago francisco <iagofranciscoifg@gmail.com>
2018-12-28 19:49:24 -0200

    Delete exercicio2.py

--- exercicio2.py
+++
@@ -1,5 +0,0 @@
-#enunciado
-print('Escreva um programa que leia um valor em metros e o exiba convertido em milímetros')
-
-A = int(input('Digite um valor em metros:'))#valor em metros
-print(A,'metros =',A*1000,'milimetros')#1 metro = 1000milimetros
