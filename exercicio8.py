d75a0f5874443c99d4923bd380e4d39e5805fd8f
iago francisco <iagofranciscoifg@gmail.com>
2018-12-28 19:48:04 -0200

    Delete exercicio8.py

--- exercicio8.py
+++
@@ -1,7 +0,0 @@
-#enunciado
-print('''Converta uma temperatura digitada em Fahrenheit para Celsius.''')
-
-f=int(input("Digite o valor da temperatura em Fahrenheit:")) #temperatura em celsius
-
-C = ((f-32)/9)*5#conversao para celsius (F = 9*C/5 + 32)
-print("A temperatura e de",C,"Celsius")
