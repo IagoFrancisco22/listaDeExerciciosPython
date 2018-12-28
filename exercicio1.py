3cbfae64e27232c16a63be6db715349663bd1821
iago francisco <iagofranciscoifg@gmail.com>
2018-12-28 19:31:31 -0200

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,6 @@
+#enunciado
+print('Faça um programa que peça dois números inteiros e imprima a soma desses dois números')
+
+A = int(input('Digite o primeiro numero:'))#primeiro numero
+B = int(input('Digite o segundo numero:'))#segundo numero
+print(A,"+",B,"=",A+B)#soma e resultado
